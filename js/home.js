e74d9e771e84f79f8d1d5fd571c7ff42655bfc14
Benjamin Boehm <bigbenni2002@gmail.com>
2024-01-09 09:50:27 +0100

    Änderungen Home

--- js/home.js
+++ js/home.js
@@ -1,3 +1,25 @@
+// Funktionen im globalen Kontext definieren
+function closePopup() {
+    document.getElementById("newsletter-popup").style.display = "none";
+}
+
+function showRegistrationPopup() {
+    document.getElementById("registration-popup").style.display = "flex";
+}
+
+function closeRegistrationPopup() {
+    document.getElementById("registration-popup").style.display = "none";
+}
+
+function showConfirmationPopup() {
+    document.getElementById("confirmation-popup").style.display = "flex";
+}
+
+function closeConfirmationPopup() {
+    document.getElementById("confirmation-popup").style.display = "none";
+}
+
+// DOMContentLoaded-Event-Listener
 document.addEventListener("DOMContentLoaded", function () {
    
     document.getElementById("newsletter-popup").style.display = "flex";
@@ -23,34 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.getElementById('registration-form').addEventListener('submit', function (event) {
+        console.log("Form submit event captured");
         event.preventDefault();
         showConfirmationPopup();
-    });
-
+    });    
+    
     document.getElementById('close-registration-btn').addEventListener('click', closeRegistrationPopup);
     document.getElementById('cancel-registration-btn').addEventListener('click', closeRegistrationPopup);
 
     document.getElementById('close-confirmation-btn').addEventListener('click', closeConfirmationPopup);
     document.getElementById('ok-confirmation-btn').addEventListener('click', closeConfirmationPopup);
 });
-
-function closePopup() {
-    document.getElementById("newsletter-popup").style.display = "none";
-}
-
-function showRegistrationPopup() {
-    document.getElementById("registration-popup").style.display = "flex";
-    console.log("Registration");
-}
-
-function closeRegistrationPopup() {
-    document.getElementById("registration-popup").style.display = "none";
-}
-
-function showConfirmationPopup() {
-    document.getElementById("confirmation-popup").style.display = "flex";
-}
-
-function closeConfirmationPopup() {
-    document.getElementById("confirmation-popup").style.display = "none";
-}
