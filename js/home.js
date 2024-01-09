fc9e57efea97e4a33e01f00ce436b484dddbbf46
Benjamin Boehm <bigbenni2002@gmail.com>
2024-01-09 09:40:15 +0100

    startseite

---
+++ js/home.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+    document.getElementById("newsletter-popup").style.display = "flex";
+
+    document.getElementById("subscribe-btn").addEventListener('click', function () {
+        closePopup();
+        showRegistrationPopup();
+    });
+
+    document.querySelector('.modal-overlay').addEventListener('click', function (event) {
+        if (event.target === this) {
+            closePopup();
+        }
+    });
+
+    document.getElementById("no-thanks-btn").addEventListener('click', function () {
+        closePopup();
+    });
+
+    document.getElementById('registration-form').addEventListener('input', function () {
+        var isValid = this.checkValidity();
+        document.getElementById('confirm-registration-btn').disabled = !isValid;
+    });
+
+    document.getElementById('registration-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        showConfirmationPopup();
+    });
+
+    document.getElementById('close-registration-btn').addEventListener('click', closeRegistrationPopup);
+    document.getElementById('cancel-registration-btn').addEventListener('click', closeRegistrationPopup);
+
+    document.getElementById('close-confirmation-btn').addEventListener('click', closeConfirmationPopup);
+    document.getElementById('ok-confirmation-btn').addEventListener('click', closeConfirmationPopup);
+});
+
+function closePopup() {
+    document.getElementById("newsletter-popup").style.display = "none";
+}
+
+function showRegistrationPopup() {
+    document.getElementById("registration-popup").style.display = "flex";
+    console.log("Registration");
+}
+
+function closeRegistrationPopup() {
+    document.getElementById("registration-popup").style.display = "none";
+}
+
+function showConfirmationPopup() {
+    document.getElementById("confirmation-popup").style.display = "flex";
+}
+
+function closeConfirmationPopup() {
+    document.getElementById("confirmation-popup").style.display = "none";
+}
