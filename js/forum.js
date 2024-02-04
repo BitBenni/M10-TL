b7c4438382f292e635998c6a8651aba7e594637a
uebersinnlos <gpt@posteo.de>
2024-02-04 18:47:47 +0000

    update forum

--- js/forum.js
+++ js/forum.js
@@ -6,26 +6,26 @@
 document.getElementById('addPostBtn').addEventListener('click', function() { // Ereignislistener für den Klick auf den "Beitrag hinzufügen"-Button hinzufügen
 
     // Elemente aus dem DOM abrufen
-    var nameInput = document.getElementById('nameInput');
-    var postInput = document.getElementById('postInput');
-    var forumPosts = document.getElementById('forumPosts');
+    let nameInput = document.getElementById('nameInput');
+    let postInput = document.getElementById('postInput');
+    let forumPosts = document.getElementById('forumPosts');
 
     if (nameInput.value.trim() !== '' && postInput.value.trim() !== '') { // Überprüfen, ob sowohl der Name als auch der Beitrag Eingaben enthalten
-        var postDiv = document.createElement('div'); // Neues Div-Element für den Beitrag erstellen 
+        let postDiv = document.createElement('div'); // Neues Div-Element für den Beitrag erstellen 
         postDiv.className = 'post'; // Klasse zuweisen
         
-        var postInfoDiv = document.createElement('div');  // Neues Div-Element für die Informationen des Beitrags erstellen 
+        let postInfoDiv = document.createElement('div');  // Neues Div-Element für die Informationen des Beitrags erstellen 
         postInfoDiv.className = 'post-info'; // Klasse zuweisen
 
-        var nameHeader = document.createElement('h3'); // Neues h3-Element für den Namen erstellen 
+        let nameHeader = document.createElement('h3'); // Neues h3-Element für den Namen erstellen 
         nameHeader.textContent = nameInput.value; // Textinhalt setzen
 
         postInfoDiv.appendChild(nameHeader); // Das h3-Element dem Div für die Beitragsinformationen hinzufügen
         
-        var postContentDiv = document.createElement('div'); // Neues Div-Element für den Inhalt des Beitrags erstellen
+        let postContentDiv = document.createElement('div'); // Neues Div-Element für den Inhalt des Beitrags erstellen
         postContentDiv.className = 'post-content'; // Klasse zuweisen
 
-        var postContent = document.createElement('p');  // Neues p-Element für den Beitragstext erstellen und Textinhalt setzen
+        let postContent = document.createElement('p');  // Neues p-Element für den Beitragstext erstellen und Textinhalt setzen
         postContent.textContent = postInput.value;  // Textinhalt setzen
 
         postContentDiv.appendChild(postContent);  // Das p-Element dem Div für den Beitragsinhalt hinzufügen
