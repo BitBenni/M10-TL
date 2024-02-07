f297df7051823e38f42641be2f23fbdbea7bffef
Benjamin Boehm <bigbenni2002@gmail.com>
2024-02-07 15:49:27 +0100

    Auslagerung und erstes CSS

--- js/datatables.js
+++ js/datatables.js
@@ -14,3 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+$(document).ready(function() {
+    // Hier wird die JSON-Datei geladen
+
+    $.ajax({ // AJAX steht für "Asynchronous JavaScript and XML" und ist eine Technik, die es ermöglicht, 
+            // Daten zwischen einem Webbrowser und einem Webserver im Hintergrund auszutauschen, ohne die gesamte Seite neu zu laden
+
+        url: './json/data.json', // URL der JSON-Datei
+        dataType: 'json', // Datentyp der Datei (hier JSON)
+        success: function(jsonData) {
+            // Erstelle DataTable
+            $('#example').DataTable({ // Darstellung der Tabelle mit CSS: Example 
+                data: jsonData,
+                columns: [
+                    { data: 'id' }, // Attribute JSON
+                    { data: 'name' },
+                    { data: 'email' }
+                ]
+            });
+        },
+        error: function(error) { // wenn JSON nicht vorhanden gebe Error in der Console aus
+            console.error('JSON Datei existiert nicht:', error);
+        }
+    });
+});
